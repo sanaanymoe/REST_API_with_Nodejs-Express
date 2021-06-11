65feb0a7ba28cd2f7a9d7d0cde35313d310321f9
sanaanymoe <sanaany1992@gmail.com>
2021-06-11 00:20:10 -0700

    create transactions and spend routes

--- index.js
+++ index.js
@@ -1,16 +1,52 @@
 const express = require('express');
 
-const app = express()
+const app = express();
+app.use(express.json());
+
 
 // app.get()
 // app.post()
 // app.put()
 // app.delete()
 
+let transactions = [];
+let payerPoints = {};
+
+app.post('/api/transactions' , (req, res) => {
+
+    let { payer, points } = req.body;
+    if (!(payer && points) && points > 0){
+        res.status(400).send('Payer must be entered and amount must be greater than zero')
+    }
+
+    let transaction = {
+        payer,
+        points,
+        'timestamp': new Date()
+    }
+    // console.log("result ", transaction );
+
+    transactions.push(transaction);
+    console.log('trans', transactions)
+    res.status(200).send('success')
+
+})
 app.get('/', (req, res) => {
     res.send('hello world'.toUpperCase())
 });
 
+app.get('/api/spend/:amount', (req, res) => {
+    console.log('amt', req.params.amount);
+    let amt = req.params.amount;
+
+    if (typeof(amt) !== 'number'){
+        res.status(400).send('Amount must be a number')
+    }
+
+    for (let transaction of transactions){
+        
+    }
+});
 app.get('/api/customers', (req, res) => {
     res.send([1, 2, 3])
 });
