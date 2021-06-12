4db3b34daae6cbc57b9dfd0419c9d116242220c3
sanaanymoe <sanaany1992@gmail.com>
2021-06-11 20:17:17 -0700

    add routes to show transactions and spend points

--- index.js
+++ index.js
@@ -4,58 +4,85 @@ const app = express();
 app.use(express.json());
 
 
-// app.get()
-// app.post()
-// app.put()
-// app.delete()
-
 let transactions = [];
-let payerPoints = {};
 
+// ---------------- Splash page -------------
+
+app.get('/', (req, res) => {
+    res.send('FETCH REWARDS IS THE BEST')
+});
+
+//Add a transaction route
 app.post('/api/transactions' , (req, res) => {
 
+    // ------- deconstrcting the values of payer and point from the request's body 
     let { payer, points } = req.body;
+
+    // Check if there is any missing key on the request 
     if (!(payer && points) && points > 0){
         res.status(400).send('Payer must be entered and amount must be greater than zero')
     }
-
+    //Build a transaction off of the request then add it to the transactions 
     let transaction = {
         payer,
         points,
         'timestamp': new Date()
     }
-    // console.log("result ", transaction );
 
     transactions.push(transaction);
-    console.log('trans', transactions)
-    res.status(200).send('success')
 
+    //send a success message aftersuccessfully adding a transaction 
+    res.status(200).send('success')
 })
-app.get('/', (req, res) => {
-    res.send('hello world'.toUpperCase())
+
+//To display all transactions 
+app.get("/api/transactions", (req, res) => {
+  res.send(transactions);
 });
 
-app.get('/api/spend/:amount', (req, res) => {
-    console.log('amt', req.params.amount);
-    let amt = req.params.amount;
+//Spend points route
+app.post('/api/spend', (req, res) => {
+    // console.log('amt', req.body);
+    let amt = req.body.amount;
+    let response = [];
 
+    
+    // The value of amount must be a number 
     if (typeof(amt) !== 'number'){
         res.status(400).send('Amount must be a number')
+    } else {
+        while(amt > 0){
+
+            for (let transaction of transactions){
+                if (transaction.points === 0 ) continue;
+                let diff = Math.abs(amt - transaction.points);
+
+                let obj = {"payer": `${transaction.payer}` , "points": 0}
+    
+                if (amt < transaction.points){
+                    transaction.points -= amt;
+                    obj.points = - amt;
+                    amt = 0;
+            
+                }else {
+                    amt -= transaction.points;
+            
+                    obj.points = - transaction.points;
+                    transaction.points = 0;
+    
+                }
+                response.push(obj);
+            }
+        }
     }
 
-    for (let transaction of transactions){
-        
-    }
-});
-app.get('/api/customers', (req, res) => {
-    res.send([1, 2, 3])
+    //after successfully using the points we send a status 200 with the response showing the points that were spend
+
+    res.status(200).send(response);
+
 });
 
-app.get('/api/customers/:id', (req, res) => {
-    res.send(req.params.id);
-});
-
-//make port number dynamic
+//make port number dynamic (if the PORT number wasn't set then port 3000 will be used by default)
 
 const port = process.env.PORT || 3000
 app.listen(port, () => {
